3d325efee273d3c6965e32082a53f37124640632
STRG-Z <110018726+PyrexHitech25@users.noreply.github.com>
2026-01-05 12:51:02 +0100

    Update messy_sales_data_analysis.sql

--- 2.MySQL/messy_sales_data_analysis.sql
+++ 2.MySQL/messy_sales_data_analysis.sql
@@ -1,11 +1,38 @@
 SELECT * FROM messy_sales_db.messy_sales_data_cleaned;
 
+ALTER TABLE messy_sales_db.messy_sales_data_cleaned
+CHANGE order_id order_number VARCHAR(20);
+
+ALTER TABLE messy_sales_db.messy_sales_data_cleaned
+CHANGE `index` id VARCHAR(20);
+
+CREATE DATABASE messy_sales_data_analysis;
 USE messy_sales_data_analysis;
 
-CREATE TABLE dim_region (
+CREATE TABLE dim_sales_details (
 	region_id INT AUTO_INCREMENT PRIMARY KEY,
     region_date DATE,
-    person VARCHAR(20) UNIQUE
+    person VARCHAR(20) UNIQUE,
+    sales_product VARCHAR(20) UNIQUE,
 );
-    
-    
+
+
+
+
+
+
+CREATE TABLE report_analysis (
+	analysis_id INT AUTO_INCREMENT PRIMARY KEY,
+    min_total_price DECIMAL(10,2), 
+    max_total_price DECIMAL(10,2),
+    max_unit_price DECIMAL(10,2),
+    min_unit_price DECIMAL(10,2)
+);
+
+INSERT IGNORE INTO report_analysis (min_total_price, max_total_price, min_unit_price, max_unit_price)
+SELECT 
+    MIN(total_price),
+    MAX(total_price),
+    MIN(unit_price),
+    MAX(unit_price)
+FROM messy_sales_db.messy_sales_data_cleaned
